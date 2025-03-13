e07958cbe4afc486d91fdc3eff6fd4d8422de2b9
JT Perry <jt@spicelabs.io>
2025-03-13 16:32:19 -0700

    Issue #1 further expand shell script to indicate whether adg or upload was called

--- grind.sh
+++ grind.sh
@@ -16,14 +16,16 @@ Help()
    echo "upload:: packages to a Wasabi server using security keys supplied"
    echo ""
    echo "options:"
-   echo "b     For adg, the source directory or file to scan"
-   echo "j     For upload, the JWT token for uploading to Wasabi"
-   echo "k     For upload, the key for encrypting files for uploading to Wasabi"
-   echo "o     For adg, directory that output should be directed to"
-   echo "p     For upload, directory or file that should be uploaded to Wasabi"
-   echo "s     For upload, the server address/endpoint to upload to"
-   echo "z     For upload, the path to a zip file that contains security credentials for Wasabi"
-   echo
+   echo "-b     For adg, the source directory or file to scan"
+   echo "-c     Command either 'adg' or 'upload'"
+   echo "-j     For upload, the JWT token for uploading to Wasabi"
+   echo "-k     For upload, the key for encrypting files for uploading to Wasabi"
+   echo "-o     For adg, directory that output should be directed to"
+   echo "-p     For upload, directory or file that should be uploaded to Wasabi"
+   echo "-s     For upload, the server address/endpoint to upload to"
+   echo "-z     For upload, the path to a zip file that contains security credentials for Wasabi"
+   echo;
+   exit 1;
 }
 
 ############################################################
@@ -34,18 +36,47 @@ Help()
 ############################################################
 # Process the input options. Add options as needed.        #
 ############################################################
+
+#Setup some variables
+#adg build directory
+#BUILDDIR = " "
+#command to run
+#COMMAND = " "
+#jwt token for upload
+#JWT = " "
+#public key for upload
+#PUBLICKEY = " "
+#output directory for adg
+#OUTPUTDIR = " "
+#upload dir
+#UPLOADDIR = " "
+#wasabi server
+#SERVERNAME = " "
+#zip file path with creditials
+#ZIPFILEPATH = " "
+
 # Get the options
-while getopts ":bhjkopsz:" option; do
+while getopts ":b:c:hj:k:o:p:s:z:" option; do
    case $option in
+      c) 
+         command=${OPTARG} >&2
+
+         #it needs to be "adg" or "upload" or call the help
+         #if [ "$command" != "adg" ] && [ "$command" != "upload" ] then
+         if [[ "$command" != "adg" ]] && [[ "$command" != "upload" ]]; then
+            echo "Command needs to be either adg or upload"
+            Help
+         fi
+         ;;
       h) # display Help
          Help
          exit;;
      \?) # Invalid option
          echo "Error: Invalid option"
          Help
-         exit;;
+         exit 1;;
    esac
 done
 
 
-echo "do some work"
+echo "do the "$command""
